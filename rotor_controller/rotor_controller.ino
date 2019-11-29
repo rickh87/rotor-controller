60a04a31cc2944dac3004056f47408e6c06a5fe9
Rick Holmes <rickh877@gmail.com>
2019-11-28 19:08:38 -0500

    added initialization code for direction LEDS

--- rotor_controller/rotor_controller.ino
+++ rotor_controller/rotor_controller.ino
@@ -10,10 +10,35 @@
   pinMode(29, OUTPUT);
   pinMode(30, OUTPUT);
   pinMode(31, OUTPUT);
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(34, OUTPUT);
+  pinMode(35, OUTPUT);  
+  pinMode(36, OUTPUT);
   digitalWrite(28, LOW);
   digitalWrite(29, LOW);
   digitalWrite(30, LOW);
   digitalWrite(31, LOW);
+  digitalWrite(32, HIGH);
+  Serial.println("HOME");
+  delay(1000);
+  digitalWrite(32, LOW);
+  digitalWrite(33, HIGH);
+  Serial.println("DWN");
+  delay(1000);
+  digitalWrite(33, LOW);
+  digitalWrite(34, HIGH);
+  Serial.println("CW");
+  delay(1000);
+  digitalWrite(34, LOW);
+  digitalWrite(35, HIGH);
+  Serial.println("UP");
+  delay(1000);
+  digitalWrite(35, LOW);
+  digitalWrite(36, HIGH);
+  Serial.println("CCW");
+  delay(1000);
+  digitalWrite(36, LOW);
  }
 
  void loop() {
